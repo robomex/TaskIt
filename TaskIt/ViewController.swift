d2717ef79d7b349bc89db8f7394dfbcef658bff1
robomex <atmorales@gmail.com>
2015-04-01 23:21:56 -0500

    Xcode 6.1 updates, fixed bug to allow task section swaps

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -98,11 +98,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
-            return "To Do"
+        
+        if fetchedResultsController.sections?.count == 1 {
+            let fetchedObjects = fetchedResultsController.fetchedObjects!
+            let testTask:TaskModel = fetchedObjects[0] as TaskModel
+            
+            if testTask.completed == true {
+                return "Completed"
+            } else {
+                return "To do"
+            }
+            
         } else {
-            return "Completed"
+            if section == 0 {
+                return "To Do"
+            } else {
+                return "Completed"
+            }
         }
+        
+        
     }
     
     
@@ -110,11 +125,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let thisTask = fetchedResultsController.objectAtIndexPath(indexPath) as TaskModel
         
-        if indexPath.section == 0 {
-            thisTask.completed = true
-        
-        } else {
+        if thisTask.completed == true {
             thisTask.completed = false
+        } else {
+            thisTask.completed = true
         }
         
         (UIApplication.sharedApplication().delegate as AppDelegate).saveContext()
